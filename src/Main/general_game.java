8a2bd0817a9b47bd94e87e9f736444144eb8be11
Haley Vong <haleyvong@gmail.com>
2024-03-10 15:13:51 -0500

    sprint 2 prod code completed

--- src/Main/general_game.java
+++ src/Main/general_game.java
@@ -4,12 +4,17 @@ package Main;
 public class general_game extends SOS_board{
 	
 	public general_game(int size){
-		super(size);
+		super(size);   //call the constructor of the superclass SOS_board
 	}
 	
+	
+	//override the checkForWin method to implement the win conditions
+	//for the general game
 	public void checkForWin() {
-		int maxPossibleMoves = board_size * board_size;
+		int maxPossibleMoves = board_size * board_size;   //calculate the max possible moves
 		
+		//check if there are remaining moves
+		//and change the game state
 		if(totalMoves < maxPossibleMoves)
 			currentGameState = GameState.PLAYING;
 		else if (bluePoints > redPoints)
--- src/Main/simple_game.java
+++ src/Main/simple_game.java
@@ -5,13 +5,17 @@ import Main.SOS_board.GameState;
 public class simple_game extends SOS_board{
 	
 	public simple_game(int size) {
-		super(size);
+		super(size);   //call the constuctor of the superclass SOS_board
 	}
 	
+	//override the checkForWin method to implement the win conditions
+	//for the simple game
 	public void checkForWin() {
-		int pointsToWin = 1; 
-		int maxPossibleMoves = board_size * board_size;
+		int pointsToWin = 1;    //# of points to win a simple game
+		int maxPossibleMoves = board_size * board_size;  //calculate the max possible moves
 		
+		
+		//change game state based on how many points the players each have
 		if(bluePoints >= pointsToWin)
 			currentGameState = GameState.B_WON;
 		else if (redPoints >= pointsToWin)
